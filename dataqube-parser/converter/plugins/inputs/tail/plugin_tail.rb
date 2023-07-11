6b1a96d6d4da02ac03279eb81b9f5f10a6204509
Phares Chakour <phares-p.chakour@external.thalesgroup.com>
2023-07-11 18:41:59 +0200

    WIP: Adding format for input tail

--- dataqube-parser/converter/plugins/inputs/tail/plugin_tail.rb
+++ dataqube-parser/converter/plugins/inputs/tail/plugin_tail.rb
@@ -15,10 +15,23 @@ class Tail < Dataqube::Input
         path #{params[:path]}
         tag #{params[:tag]}
         read_from_head true
+        path_key filepath
         <parse>
           @type none
         </parse>
       </source>
+      <filter *>
+        @type dataqube
+        init ''
+        code \"${
+          if record.key?('filepath') && record['filepath'].end_with?('.json')
+            json_result = parse_json(record['message'])
+            record = merge_hash(record, json_result)
+            record['message'] = json_result['message']
+          end
+          record
+        }\"
+      </filter>
     "
     else
       ""
