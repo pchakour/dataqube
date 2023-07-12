b2ca48110f70d7f144b2ff1081253ccd50294149
Phares Chakour <phares-p.chakour@external.thalesgroup.com>
2023-07-12 10:41:57 +0200

    Add format for plugin tail

--- dataqube-parser/converter/plugins/inputs/tail/plugin_tail.rb
+++ dataqube-parser/converter/plugins/inputs/tail/plugin_tail.rb
@@ -2,6 +2,7 @@ require_relative '../../../core/input'
 
 class Tail < Dataqube::Input
   config_param :path, :string
+  config_param :format, ['auto', 'raw', 'json'], default: 'auto'
 
   def initialize()
     super("tail")
@@ -9,7 +10,7 @@ class Tail < Dataqube::Input
 
   def raw(parser, params)
     if parser === :fluentd
-      "
+      conversion = "
       <source>
         @type tail
         path #{params[:path]}
@@ -17,22 +18,28 @@ class Tail < Dataqube::Input
         read_from_head true
         path_key filepath
         <parse>
-          @type none
+          @type #{params[:format] === 'auto' || params[:format] === 'raw' ? 'none' : params[:format]}
         </parse>
       </source>
-      <filter *>
-        @type dataqube
-        init ''
-        code \"${
-          if record.key?('filepath') && record['filepath'].end_with?('.json')
-            json_result = parse_json(record['message'])
-            record = merge_hash(record, json_result)
-            record['message'] = json_result['message']
-          end
-          record
-        }\"
-      </filter>
     "
+
+      if params[:format] === 'auto'
+        conversion << "
+        <filter *>
+          @type dataqube
+          init ''
+          code \"${
+            if record.key?('filepath') && record['filepath'].end_with?('.json')
+              json_result = parse_json(record['message'])
+              record = merge_hash(record, json_result)
+              record['message'] = json_result['message']
+            end
+            record
+          }\"
+        </filter>
+      "
+      end
+      return conversion
     else
       ""
     end
